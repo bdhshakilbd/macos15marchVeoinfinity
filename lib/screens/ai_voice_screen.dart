18472517f992d2f9fed1e7d5e62bc6e383d2e939
bdhshakilbd <bdhshakilbd@gmail.com>
2026-03-15 22:45:13 +0600

    Fix: add missing path import in ai_voice_screen.dart

--- lib/screens/ai_voice_screen.dart
+++ lib/screens/ai_voice_screen.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 import 'dart:convert';
 import 'dart:io';
+import 'package:path/path.dart' as path;
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:google_fonts/google_fonts.dart';
